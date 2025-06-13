98f019fdf9e69964ff5b11cc9b429b0280e525da
qsecer <dimagolovin169@gmail.com>
2025-06-13 05:07:01 +0500

    Start
    Signed-off-by: qsecer <dimagolovin169@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-//Тут импорты стилей
-import "../src/styles/main.scss";
+import './styles/main.scss';
 import logo from './assets/Logo.svg';
-// Тут импорты компонентов
+
 import TicketsList from "./components/TicketsList.jsx";
 import Tabs from "./components/Tabs.jsx";
 import Aside from "./components/Aside.jsx";
 import Btn from "./components/Button.jsx";
 
-
 const App = () => {
+    return (
+        <div className='app'>
+            <header>
+                <img className='header--img' src={logo} alt="Logo" />
+            </header>
+            <aside className="filters-menu">
+                <Aside />
+            </aside>
+            <main className="tickets--menu">
+                <Tabs />
+                <TicketsList />
+                <Btn />
+            </main>
+        </div>
+    );
+};
 
-      return (
-              <div className='app'>
-                  <header>
-                      <img className='header--img' src={logo} alt='logo'/>
-                  </header>
-                  <aside className="filters-menu">
-                      <Aside/>
-                  </aside>
-                  <main className="tickets--menu">
-                      <Tabs/>
-                      <TicketsList/>
-                      <Btn/>
-                  </main>
-              </div>
-      )
-}
 export default App;
